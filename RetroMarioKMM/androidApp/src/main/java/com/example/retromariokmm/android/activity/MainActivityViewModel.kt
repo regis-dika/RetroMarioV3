3dc17776a55828c8eb42d67a4fca465a580ae729
roefwin <regis-971@hotmail.coml>
2023-05-13 14:26:23 +0200

    update user to null on logout

--- RetroMarioKMM/androidApp/src/main/java/com/example/retromariokmm/android/activity/MainActivityViewModel.kt
+++ RetroMarioKMM/androidApp/src/main/java/com/example/retromariokmm/android/activity/MainActivityViewModel.kt
@@ -43,7 +43,8 @@ class MainActivityViewModel @Inject constructor(
                         is Error -> ERROR
                         is Loading -> PENDING
                         is Success -> SUCCESS
-                    }
+                    },
+                    retroUser = null
                 )
             }
         }
